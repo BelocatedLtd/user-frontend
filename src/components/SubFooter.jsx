a133182d75d6ae5d8cd464226ec6411b2dfac7a0
codecjay <mail2jhenry@gmail.com>
2023-05-29 17:10:32 +0100

    editted the homepage small

--- src/components/SubFooter.jsx
+++ src/components/SubFooter.jsx
@@ -12,7 +12,7 @@ const SubFooter = () => {
             
             <div className='w-full p-6 text-gray-600'>
                 <div className='flex items-center gap-2 mb-6'>
-                    <h1 className='text-2xl text-secondary'>Categories</h1>
+                    <h1 className='text-2xl text-secondary'>Belocated</h1>
                     <AiOutlineHolder className='rotate-90 text-2xl text-tertiary mt-1'/>
                 </div> 
                 <ul className='text-lg' key="category.id">
@@ -39,16 +39,14 @@ const SubFooter = () => {
 
             <div className='w-full p-6 text-gray-600'>
                 <div className='flex items-center gap-2 mb-6'>
-                    <h1 className='text-2xl text-secondary'>Auctions</h1>
+                    <h1 className='text-2xl text-secondary'>Information</h1>
                     <AiOutlineHolder className='rotate-90 text-2xl text-tertiary mt-1'/>
                 </div> 
                         <ul className='text-lg flex flex-col gap-2'>
-                            <li>Live Auctions</li>
-                            <li>Upcoming Auctions</li>
-                            <li>Expired Auctions</li>
-                            <li>Top Winners</li>
-                            <li>Trending Auctions</li>
-                            
+                            <li>About Us</li>
+                            <li>Contact Us</li>
+                            <li>FAQ</li>
+                            <li>Top Partners</li>
                         </ul>
             </div>
 
@@ -59,7 +57,7 @@ const SubFooter = () => {
                 </div> 
                         <ul className='text-lg flex flex-col gap-3'>
                             <li className='flex items-center gap-2'><BsFillTelephoneFill className='text-tertiary border border-tertiary rounded-full p-1 text-2xl'/> +234 803 555 4444</li>
-                            <li className='flex items-center gap-2'><MdOutlineEmail  className='text-tertiary border border-tertiary rounded-full p-1 text-2xl'/>cs@auctioncube.com</li>
+                            <li className='flex items-center gap-2'><MdOutlineEmail  className='text-tertiary border border-tertiary rounded-full p-1 text-2xl'/>cs@belocated.com</li>
                             <li className='flex items-center gap-2'><MdLocationPin  className='text-tertiary border border-tertiary rounded-full p-1 text-2xl'/>222 Chevron Estate, VI, Lagos</li>
                         </ul>
             </div>
