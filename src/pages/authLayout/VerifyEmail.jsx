801268dab956357f28bd81fdbff43f9ed90017ec
codecjay <mail2jhenry@gmail.com>
2023-06-13 09:34:07 +0100

    fixed verification resend

--- src/pages/authLayout/VerifyEmail.jsx
+++ src/pages/authLayout/VerifyEmail.jsx
@@ -7,15 +7,49 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const VerifyEmail = () => {
-  const location = useLocation();
-  const { formData } = location.state || {};
+    const location = useLocation();
+    const { formData } = location.state || {};
     const [isLoading, setIsLoading] = useState(false)
+    const [timer, setTimer] = useState(10);
+    const [resendBtn, setResendBtn] = useState(false)
+
+    let interval;
+
+  useEffect(() => {
+    if (timer === 0) {
+      setResendBtn(true)
+      clearInterval(interval);
+      return
+    }
+
+    interval = setInterval(() => {
+      setTimer((prevTimer) => prevTimer - 1);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [timer])
 
     const {email} = formData || {};
 
-    const handleResendEmail = async() => {
-        const response = await resendVerificationEmail(email)
-    }
+    const handleResendEmail = async (e) => {
+      e.preventDefault()
+ 
+     try {
+         setIsLoading(true)
+      const response = await resendVerificationEmail(email) 
+      if (response) {
+         toast.success(`Verification link sent to ${email}`)
+         
+         setResendBtn(false)
+         setTimer(10)
+ 
+      }
+       setIsLoading(false)
+     } catch (error) {
+       setIsLoading(false)
+       console.log(error)
+     }
+     }
 
     return (
         <div className='w-full h-[88vh] flex items-center'>
@@ -28,7 +62,10 @@ const VerifyEmail = () => {
                     <CheckmarkIcon />
                 </h3>
                 <p className='w-[500px] my-[1rem] text-center px-6'>Please check the email address {email} for instructions to verify your belocated account</p>
-                <button onClick={() => handleResendEmail()} type="submit" className='w-[60%] mt-1 py-2 text-md rounded-xl bg-secondary text-gray-100 mb-5'>Resend Email</button>
+                <div className='flex items-center gap-2'>
+                    {resendBtn && <button onClick={handleResendEmail} className='w-full mt-1  py-2 text-md rounded-xl bg-tertiary text-gray-100 text-center px-6 mb-5'>Resend Verification Link</button>}
+                    {!resendBtn && <span className='ml-4 p-3 bg-slate-100 text-gray-800 rounded-full'>Resend in: {timer}</span> }
+                  </div>
             </div>
           </div>
         </div>
