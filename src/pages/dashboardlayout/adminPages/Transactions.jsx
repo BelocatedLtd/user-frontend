bbc3192d6370ca2b7d3a02ecca9eaf28cb41306f
codecjay <mail2jhenry@gmail.com>
2023-06-17 17:46:35 +0100

    fixed responsiveness

--- src/pages/dashboardlayout/adminPages/Transactions.jsx
+++ src/pages/dashboardlayout/adminPages/Transactions.jsx
@@ -1,8 +1,87 @@
 import React from 'react'
+import DataTable from 'react-data-table-component';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { selectTransactions, selectIsError, selectIsLoading } from '../../../redux/slices/transactionSlice';
 
 const Transactions = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const users = useSelector(selectTransactions)
+  const isLoading = useSelector(selectIsLoading)
+  const isError = useSelector(selectIsError)
+
+  const columns = [
+    {
+      name: 'S/N', 
+      selector: row => row.trxId,
+    },
+    {
+      name: 'User',
+      cell: (row) => (
+        <button className='bg-[#18141E] text-gray-100 px-3 py-2 rounded-2xl hover:bg-btn' onClick={(e) => handleButtonClick(e, row._id)}>View User</button>
+      )
+    },
+    {
+      name: 'Transaction Type',
+      selector: row => row.trxType,
+    },
+    {
+      name: 'Amount',
+      selector: row => row.chargedAmount,
+      sortable: true
+    },
+    {
+      name: 'Date',
+      selector: row => row.date,
+  
+    },
+    {
+      name: 'Status',
+      selector: row => row.status, 
+      sortable: true
+    },
+  ];
+
+  const customStyles = {
+    headCells: {
+      style: {
+        backgroundColor: '#18141E',
+        color: '#f4f4f4',
+        fontSize: '15px'
+      }
+    },
+  }
+
+  const handleButtonClick = (e, userId) => {
+    e.preventDefault();
+    navigate(`/admin/dashboard/user/${userId}`)
+  }
+
+   useEffect(() => {
+    dispatch(handleGetAllUser())
+
+    if (isError) {
+      toast.error("failed to fetch users")
+    }
+
+}, [isError, dispatch])
+
+
   return (
-    <div>Transactions</div>
+        <div className='w-full mx-auto mt-[2rem]'>
+        <DataTable 
+        columns={columns} 
+        data={users}
+        progressPending={isLoading}
+        pagination
+        selectableRows
+        fixedHeader
+        customStyles={customStyles}
+        //handleButtonClick={handleButtonClick}
+        />
+        </div>
   )
 }
 
--- src/pages/dashboardlayout/adminPages/UserSingle.jsx
+++ src/pages/dashboardlayout/adminPages/UserSingle.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useLocation, useParams } from 'react-router-dom'
+import { selectUsers } from '../../../redux/slices/userSlice'
+import { useState } from 'react'
+import Loader from '../../../components/loader/Loader'
 
 const UserSingle = () => {
+  const {id} = useParams()
+  const location = useLocation();
+  const { user } = location.state || {};
+  //const users = useSelector(selectUsers)
+  const [userDetail, setUser] = useState()
+  const [isLoading, setIsLoading] = useState(false)
+
   return (
-    <div>UserSingle</div>
+    <div className='w-full h-fit'>
+      {isLoading && <Loader />}
+      <div className='w-[70%] h-full shadow-xl'>
+        <div>
+          <label htmlFor="fullname">Fullname</label>
+          <p>{user?.fullname}</p>
+        </div>
+        <div>
+          <label htmlFor="fullname">Username</label>
+          <p className='text-black'>{user?.username}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
