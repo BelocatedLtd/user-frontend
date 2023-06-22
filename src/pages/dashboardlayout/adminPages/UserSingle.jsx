b3020f621edfc1a43f66e0b11dc9b2c480658d76
codecjay <mail2jhenry@gmail.com>
2023-06-22 03:49:28 +0100

    fixed user bank details

--- src/pages/dashboardlayout/adminPages/UserSingle.jsx
+++ src/pages/dashboardlayout/adminPages/UserSingle.jsx
@@ -66,6 +66,7 @@ const UserSingle = () => {
       <div className='box flex flex-col border-b border-gray-100 p-3 pb-6 ml-5'>
         <div className='flex flex-col items-center gap-3 mt-3 md:flex-row'>
           <FaUser size={300} className='text-gray-800 border border-gray-100 p-[2rem] rounded-full'/>
+          {/* User Details Hero Section */}
             <div className='flex flex-col text-center gap-1 md:text-left'>
               <h3 className='text-[3rem]'>{user?.fullname ? user?.fullname : user?.username}</h3>
               <small className='text-gray-600 mt-[-0.7rem] mb-[1rem] font-semibold'>@{user?.username}</small>
@@ -95,92 +96,113 @@ const UserSingle = () => {
             </div>
         </div>
 
+        {/* User Details & Bank Account */}
         <div className='container shadow-xl py-[3rem] px-[2rem] mt-[2rem]'>
-
         {/* User Details */}
-        <div className='flex flex-col  md:flex-row'>
-          <div className='box flex flex-col p-3 pb-6'>
-            <label htmlFor="adverter" className='text-secondary text-[25px] font-bold'>User Details</label>
+        <div className='flex flex-col gap-[3rem] md:flex-row'>
+          <div className='right__main flex flex-col   p-6 md:flex-row md:border-r border-gray-100'>
+            <div className='box flex flex-col p-3 pb-6'>
+              <label htmlFor="adverter" className='text-secondary text-[25px] font-bold'>User Details</label>
 
-            <div className='user__details__container flex flex-col gap-1 md:gap-[4rem] md:flex-row'>
-              <div className='left flex flex-col gap-1 md:gap-[4rem] mt-3'>
-                <div className='flex flex-col border-b border-gray-50 py-3'>
-                  <label htmlFor="" className='font-bold'>Email:</label>
-                  <p>{user?.email}</p>
-                </div>
+              <div className='user__details__container flex flex-col gap-1 md:gap-[4rem] md:flex-row'>
+                <div className='left flex flex-col gap-1 md:gap-[4rem] mt-3'>
+                  <div className='flex flex-col border-b border-gray-50 py-3'>
+                    <label htmlFor="" className='font-bold'>Email:</label>
+                    <p>{user?.email}</p>
+                  </div>
 
-                <div className='flex flex-col border-b border-gray-50 py-3'>
-                  <label htmlFor="" className='font-bold'>Phone:</label>
-                  <p className=''>{user?.phone}</p>
-                </div>
+                  <div className='flex flex-col border-b border-gray-50 py-3'>
+                    <label htmlFor="" className='font-bold'>Phone:</label>
+                    <p className=''>{user?.phone}</p>
+                  </div>
 
-                <div className='flex flex-col border-b border-gray-50 py-3'>
-                  <label htmlFor="" className='font-bold'>Email Verification:</label>
-                  <div className='flex gap-1 items-center'>
-                    <p className={`${user?.isEmailVerified ? 'text-green-800' : 'text-red-700'}`}>{user?.isEmailVerified ? "Verified" : "Unverified"}</p>
-                    {user?.isEmailVerified ? <CheckmarkIcon /> : <MdOutlineCancel className='text-tertiary'/>}
+                  <div className='flex flex-col border-b border-gray-50 py-3'>
+                    <label htmlFor="" className='font-bold'>Email Verification:</label>
+                    <div className='flex gap-1 items-center'>
+                      <p className={`${user?.isEmailVerified ? 'text-green-800' : 'text-red-700'}`}>{user?.isEmailVerified ? "Verified" : "Unverified"}</p>
+                      {user?.isEmailVerified ? <CheckmarkIcon /> : <MdOutlineCancel className='text-tertiary'/>}
+                    </div>
+                  </div>
+
+                  <div className='flex flex-col border-b border-gray-50 py-3'>
+                    <label htmlFor="" className='font-bold'>Phone Verification:</label>
+                    <div className='flex gap-1 items-center'>
+                      <p className={`${user?.isPhoneVerified ? 'text-green-800' : 'text-red-700'}`}>{user?.isPhoneVerified ? "Verified" : "Unverified"}</p>
+                      {user?.isPhoneVerified ? <CheckmarkIcon /> : <MdOutlineCancel className='text-tertiary'/>}
+                    </div>
+                  </div>
+
+                  <div className='flex flex-col border-b border-gray-50 py-3'>
+                    <label htmlFor="" className='font-bold'>State:</label>
+                    <p>{user?.location}</p>
                   </div>
                 </div>
 
-                <div className='flex flex-col border-b border-gray-50 py-3'>
-                  <label htmlFor="" className='font-bold'>Phone Verification:</label>
-                  <div className='flex gap-1 items-center'>
-                    <p className={`${user?.isPhoneVerified ? 'text-green-800' : 'text-red-700'}`}>{user?.isPhoneVerified ? "Verified" : "Unverified"}</p>
-                    {user?.isPhoneVerified ? <CheckmarkIcon /> : <MdOutlineCancel className='text-tertiary'/>}
+                <div className='right flex flex-col gap-1 md:gap-[4rem] mt-3'>
+                  <div className='flex flex-col border-b border-gray-50 py-3'>
+                    <label htmlFor="" className='font-bold'>Gender:</label>
+                    <div className='flex gap-1 items-baseline'>
+                      <p>{user?.gender}</p>
+                    </div>
+                    
                   </div>
-                </div>
 
-                <div className='flex flex-col border-b border-gray-50 py-3'>
-                  <label htmlFor="" className='font-bold'>State:</label>
-                  <p>{user?.location}</p>
-                </div>
-              </div>
-
-              <div className='right flex flex-col gap-1 md:gap-[4rem] mt-3'>
-                <div className='flex flex-col border-b border-gray-50 py-3'>
-                  <label htmlFor="" className='font-bold'>Gender:</label>
-                  <div className='flex gap-1 items-baseline'>
-                    <p>{user?.gender}</p>
+                  <div className='flex flex-col border-b border-gray-50 py-3'>
+                    <label htmlFor="" className='font-bold'>Religion:</label>
+                    <p>{user?.religion}</p>
                   </div>
-                  
-                </div>
 
-                <div className='flex flex-col border-b border-gray-50 py-3'>
-                  <label htmlFor="" className='font-bold'>Religion:</label>
-                  <p>{user?.religion}</p>
-                </div>
-
-                <div className='flex flex-col border-b border-gray-50 py-3'>
-                  <label htmlFor="" className='font-bold'>Account Type:</label>
-                  <div className='flex gap-1 items-baseline'>
-                    <p>{user?.accountType}</p>
+                  <div className='flex flex-col border-b border-gray-50 py-3'>
+                    <label htmlFor="" className='font-bold'>Account Type:</label>
+                    <div className='flex gap-1 items-baseline'>
+                      <p>{user?.accountType}</p>
+                    </div>
                   </div>
-                </div>
 
-                <div className='flex flex-col border-b border-gray-50 py-3'>
-                  <label htmlFor="" className='font-bold'>Weekly Free Tasks:</label>
-                    <p>{user?.freeTaskCount}</p>
-                </div>
+                  <div className='flex flex-col border-b border-gray-50 py-3'>
+                    <label htmlFor="" className='font-bold'>Weekly Free Tasks:</label>
+                      <p>{user?.freeTaskCount}</p>
+                  </div>
 
-                <div className='flex flex-col border-b border-gray-50 py-3'>
-                  <label htmlFor="" className='font-bold'>LGA:</label>
-                  <p>{user?.community}</p>
+                  <div className='flex flex-col border-b border-gray-50 py-3'>
+                    <label htmlFor="" className='font-bold'>LGA:</label>
+                    <p>{user?.community}</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+
+          <div className='left__main flex-col p-6 md:flex'>
+          <label htmlFor="adverter" className='text-secondary text-[25px] font-bold'>Bank Details</label>
+          <div className='left flex flex-col gap-1 md:gap-[2rem] mt-3'>
+                  <div className='flex flex-col border-b border-gray-50 py-3'>
+                    <label htmlFor="" className='font-bold'>Bank Name:</label>
+                    <p>{user?.bankName}</p>
+                  </div>
+
+                  <div className='flex flex-col border-b border-gray-50 py-3'>
+                    <label htmlFor="" className='font-bold'>Bank Account Holder:</label>
+                    <p className=''>{user?.accountHolderName}</p>
+                  </div>
+
+                  <div className='flex flex-col border-b border-gray-50 py-3'>
+                    <label htmlFor="" className='font-bold'>Bank Account Number:</label>
+                    <p className=''>{user?.bankAccountNumber}</p>
+                  </div>
+                </div>
+          </div>
         </div>
         
-
+        
         {/* Advert Controls */}
         <div className='mt-[1rem]'>
-          
           <div className='flex gap-2'>
             <button onClick={handleDelete} className='py-2 px-5 bg-tertiary text-primary'>Delete User</button>
             <button className='py-2 px-5 bg-secondary text-primary'>Message Advertiser</button>
           </div>
         </div>
-      </div>
+        </div>
       </div>
     </div>
   )
