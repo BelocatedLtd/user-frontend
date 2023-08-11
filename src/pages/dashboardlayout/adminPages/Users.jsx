d6a1403d4f648102f0905f44bff9e5ec6fec43f9
codecjay <mail2jhenry@gmail.com>
2023-08-11 19:32:36 +0100

    empty activity feed tray

--- src/pages/dashboardlayout/adminPages/Users.jsx
+++ src/pages/dashboardlayout/adminPages/Users.jsx
@@ -5,25 +5,61 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { handleGetAllUser, selectIsError, selectIsLoading, selectUsers } from '../../../redux/slices/userSlice';
 import { useNavigate } from 'react-router-dom';
-import { MdArrowDownward } from 'react-icons/md';
+import { MdArrowDownward, MdOutlineKeyboardArrowLeft } from 'react-icons/md';
 import { toast } from 'react-hot-toast';
+import { handleGetAllActivities, selectActivities } from '../../../redux/slices/feedSlice';
+import { FaAngleDoubleRight } from 'react-icons/fa';
+import { AiFillDelete } from 'react-icons/ai';
+import { trashAllUserActivities } from '../../../services/feedService';
+import Loader from '../../../components/loader/Loader';
+import { useState } from 'react';
 
 
 const Users = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const users = useSelector(selectUsers)
+  const activities = useSelector(selectActivities)
   const isLoading = useSelector(selectIsLoading)
   const isError = useSelector(selectIsError)
   const sortIcon = <MdArrowDownward />;
+  const [activityIsLoading, setactivityIsLoading] = useState(false)
 
   useEffect(() => {
     dispatch(handleGetAllUser())
+    dispatch(handleGetAllActivities())
 
     if (isError) {
       toast.error("failed to fetch users")
     }
 }, [dispatch])
+
+const trashAllActivities = async() => {
+
+  if (activities.length < 500) {
+    toast.error('Lets allow it get to atleast 500 activities in the feed')
+    return
+  }
+
+  setactivityIsLoading(true)
+  const response = trashAllUserActivities()
+  .catch((error)=> {
+    setactivityIsLoading(false)
+    toast.error("Failed to trash users' activities")
+    console.log(error)
+  })
+  .then((res) =>  {
+    setactivityIsLoading(false)
+    console.log(res)
+  })
+  
+  toast.promise(response, {
+      loading: 'Emptying activity feed...',
+      success: <b>Activity feed emptied successfully</b>,
+      error: <b>Failed to emptying activity feed</b>
+    }
+  );
+}
 
 
   const columns = [
@@ -97,6 +133,20 @@ const Users = () => {
 
   return (
         <div className='w-full mx-auto mt-[2rem]'>
+          {activityIsLoading && <Loader />}
+          <div className='flex items-center justify-between mb-[2rem]'>
+                <div className='flex items-center'>
+                <MdOutlineKeyboardArrowLeft size={30} onClick={() => (navigate(-1))} className='mr-1'/>
+                    <p className='font-semibold text-xl text-gray-700'>Users</p>
+                </div>
+
+                <div className='flex items-center gap-2'>
+                  <label>User Activities:</label>
+                  <p>{activities.length}</p>
+                  <AiFillDelete className='text-secondary hover:text-tertiary' onClick={trashAllActivities}/>
+                </div>
+          </div>
+
         <DataTable 
         columns={columns} 
         data={users}
--- src/services/feedService.js
+++ src/services/feedService.js
@@ -7,3 +7,9 @@ export const getAllActivities = async() => {
     const response = await axios.get(`${BACKEND_URL}/api/activities`)
         return response.data        
 }
+
+// Emptying activity feed 
+export const trashAllUserActivities = async() => {
+    const response = await axios.delete(`${BACKEND_URL}/api/activities/trash`)
+        return response.data        
+}
