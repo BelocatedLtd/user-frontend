a8d4c169b1e6c6e82b3e039f557369ded0ee8c43
codecjay <mail2jhenry@gmail.com>
2023-08-21 11:02:57 +0100

    reset

--- src/components/dashboard/FreeTaskCount.jsx
+++ src/components/dashboard/FreeTaskCount.jsx
@@ -40,13 +40,14 @@ const FreeTaskCount = () => {
     // }, [])
     
     useEffect(() => {
-      // Calculate the date when the week runs out (Sunday midnight)
-      const now = new Date();
-      const daysUntilReset = (7 + 1 - now.getDay()) % 7; // Calculate days until Sunday
-      const endOfWeek = new Date(now.getTime() + daysUntilReset * 24 * 60 * 60 * 1000);
-      endOfWeek.setHours(0, 0, 0, 0);
-  
       const updateCountdown = () => {
+        const now = new Date();
+        
+        // Calculate the date when the week runs out (Sunday midnight)
+        const daysUntilReset = (7 + 1 - now.getDay()) % 7; // Calculate days until Sunday
+        const endOfWeek = new Date(now.getTime() + daysUntilReset * 24 * 60 * 60 * 1000);
+        endOfWeek.setHours(0, 0, 0, 0);
+  
         const currentTime = Date.now();
         const remainingTime = endOfWeek.getTime() - currentTime;
   
@@ -58,14 +59,25 @@ const FreeTaskCount = () => {
   
           setCountDown({ days, hours, minutes, seconds });
         } else {
-          // Perform the code that runs when the week runs out. That's resetting every users' freetaskCounter
+          // Reset the timer and start counting down from 7 days on Monday
+          const daysUntilMonday = (8 - now.getDay()) % 7; // Calculate days until Monday
+          const nextMonday = new Date(now.getTime() + daysUntilMonday * 24 * 60 * 60 * 1000);
+          nextMonday.setHours(0, 0, 0, 0);
+  
+          const resetTime = nextMonday.getTime() + 7 * 24 * 60 * 60 * 1000; // Add 7 days to reset to a week from Monday
+          const newRemainingTime = resetTime - currentTime;
+  
+          const newDays = Math.floor(newRemainingTime / (1000 * 60 * 60 * 24));
+          const newHours = Math.floor((newRemainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+          const newMinutes = Math.floor((newRemainingTime % (1000 * 60 * 60)) / (1000 * 60));
+          const newSeconds = Math.floor((newRemainingTime % (1000 * 60)) / 1000);
+  
+          setCountDown({ days: newDays, hours: newHours, minutes: newMinutes, seconds: newSeconds });
         }
       };
   
-      // Update the count down every second
       const timer = setInterval(updateCountdown, 1000);
   
-      // Clean up the timer when the component is unmounted
       return () => clearInterval(timer);
     }, []);
   
