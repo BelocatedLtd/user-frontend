0a102d9737eb2a7fec1316be2f2c29da6990b2a8
Temidayo Oladele <51400490+Temidayomichael@users.noreply.github.com>
2024-07-06 17:36:36 +0100

    ...

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@ import path from 'path'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	distDir: './dist',
 	webpack: (config, { isServer }) => {
 		config.resolve.alias['@'] = path.resolve('src')
 		return config
