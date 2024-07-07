9cb637e57da4ad6161c73de0c8dc084f8fe3f4f4
Temidayo Oladele <51400490+Temidayomichael@users.noreply.github.com>
2024-07-07 22:25:52 +0100

    dynamic page fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '@/app/index.css'
+import ReduxProvider from '@/redux/provider'
 import { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+    return (
+			<ReduxProvider>
+				<Component {...pageProps} />
+			</ReduxProvider>
+		)
+        
 }
 
 export default MyApp
