708fb1d63153cdab98903707d680729bb8bf95f3
BelocatedLtd <belocatedltd@gmail.com>
2024-10-08 15:25:50 +0100

    Update globalConfig.js

--- src/utils/globalConfig.js
+++ src/utils/globalConfig.js
@@ -1,5 +1,4 @@
 export const BACKEND_URL =
-	process.env.NEXT_PUBLIC_BACKEND_URL
-		== 'production'
+	process.env.NEXT_PUBLIC_BACKEND_URL === 'production'
 		? 'https://dolphin-app-esx2u.ondigitalocean.app'
-		: 'http://localhost:7001'
+		: 'https://dolphin-app-esx2u.ondigitalocean.app';
