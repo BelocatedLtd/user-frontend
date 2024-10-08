b0910fa023e48e780425e06d852621063f0cc79c
BelocatedLtd <belocatedltd@gmail.com>
2024-10-08 14:58:10 +0100

    Update globalConfig.js remove slash

--- src/utils/globalConfig.js
+++ src/utils/globalConfig.js
@@ -1,5 +1,5 @@
 export const BACKEND_URL =
 	process.env.NEXT_PUBLIC_BACKEND_URL
 		== 'production'
-		? 'https://dolphin-app-esx2u.ondigitalocean.app/'
+		? 'https://dolphin-app-esx2u.ondigitalocean.app'
 		: 'http://localhost:7001'
