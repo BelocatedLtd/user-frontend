393cb70f5a3adfb5604275ca260ea2b230bf0edd
BelocatedLtd <belocatedltd@gmail.com>
2024-10-10 10:25:22 +0100

    Update page.tsx task

--- src/app/dashboard/tasks/page.tsx
+++ src/app/dashboard/tasks/page.tsx
@@ -14,6 +14,8 @@ import { useDispatch } from 'react-redux'
 import TimeAgo from 'timeago-react'
 import { cn } from '../../../../helpers'
 import Loading from '../loading'
+import { setTotal, selectTotalTasks } from '@/redux/slices/taskSlice';
+
 
 const TaskList = () => {
 	const router = useRouter()
@@ -38,6 +40,7 @@ const TaskList = () => {
 			setTasks(response?.tasks)
 
 			setTotalTasks(response.totalTasks)
+		   dispatch(setTotal(Number(response.totalTasks)));
 			// setIsLoading(false)
 		} catch (error) {
 			toast.error('Failed to retrieve tasks, please reload page')
