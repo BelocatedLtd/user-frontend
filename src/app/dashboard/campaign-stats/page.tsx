c2e2a8977edd9c765f28a5c23b8c24c8e0d3d68d
BelocatedLtd <belocatedltd@gmail.com>
2024-10-10 12:07:13 +0100

    Update page.tsx redesign

--- src/app/dashboard/campaign-stats/page.tsx
+++ src/app/dashboard/campaign-stats/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+/* 'use client'
 import Button from '@/components/Button'
 import PaginatedComponent from '@/components/Pagination'
 import { getUserAdverts } from '@/services/advertService'
@@ -99,4 +99,119 @@ const CampaignStats = () => {
 	)
 }
 
+export default CampaignStatsCampaignStats */    
+'use client'
+import Button from '@/components/Button'
+import PaginatedComponent from '@/components/Pagination'
+import { getUserAdverts } from '@/services/advertService'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { toast } from 'react-hot-toast'
+import { BsFillPlusCircleFill } from 'react-icons/bs'
+import { useSelector } from 'react-redux'
+import AdItem from '../../../components/dashboard/AdItem'
+import Loader from '../../../components/loader/Loader'
+import { selectIsLoading } from '../../../redux/slices/advertSlice'
+import { selectUser } from '../../../redux/slices/authSlice'
+
+const CampaignStats = () => {
+	const user = useSelector(selectUser)
+	const isLoading = useSelector(selectIsLoading)
+	const router = useRouter()
+
+	const [totalAdverts, setTotalAdverts] = useState(0)
+	const [currentPage, setCurrentPage] = useState(1)
+	const [limit, setLimit] = useState(10)
+	const [adverts, setAdverts] = useState([])
+
+	const getAdverts = async (page: number, limit: number) => {
+		try {
+			const response = await getUserAdverts({ page, limit })
+			setAdverts(response?.adverts)
+			setTotalAdverts(response.totalAdverts)
+		} catch (error) {
+			toast.error('Failed to retrieve tasks, please reload page')
+		}
+	}
+
+	useEffect(() => {
+		getAdverts(currentPage, limit)
+	}, [currentPage, limit])
+
+	return (
+		<div className='w-full p-5 md:p-10'>
+			<Loader open={isLoading} />
+
+			{/* Header Section */}
+			<div className='flex flex-col md:flex-row items-center justify-between gap-3 py-5 border-b border-gray-200'>
+				<div className='flex flex-col'>
+					<p className='font-semibold text-xl text-gray-700'>
+						My Ad Campaigns
+					</p>
+					<small className='font-medium text-gray-500'>
+						Click <span className='text-secondary cursor-pointer'>here</span> to
+						see and monitor your adverts
+					</small>
+				</div>
+				<div className='flex items-center gap-5 mt-3 md:mt-0'>
+					<small className='hidden md:flex h-6 w-10 items-center justify-center bg-secondary rounded-full text-primary'>
+						{adverts?.length}
+					</small>
+					<Button
+						variant='solid'
+						color='danger'
+						onClick={() => router.push('/dashboard/advertise')}
+						className='flex items-center gap-1 bg-secondary text-primary rounded-full px-5 py-2 text-[12px] md:text-[15px] hover:bg-tertiary'
+					>
+						<BsFillPlusCircleFill />
+						Campaign
+					</Button>
+				</div>
+			</div>
+
+			{/* Adverts List Section */}
+			<div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8'>
+				{adverts?.length ? (
+					adverts.map((item: any) => (
+						<AdItem
+							key={item._id}
+							id={item._id}
+							date={item.createdAt}
+							title={`${item.service} on ${item?.platform?.toUpperCase()}`}
+							adperPostAmt={`${item.costPerTask} Per Ad`}
+							roi={item.desiredROI}
+							adBudget={item.adAmount}
+							adService={item.service}
+							status={item.status}
+							item={item}
+							url={item.socialPageLink}
+							user={user}
+							taskSubmitters={item.taskSubmitters}
+							completedTasksCount={item.completedTasksCount}
+							callback={() => getAdverts(currentPage, limit)}
+						/>
+					))
+				) : (
+					<p className='text-center text-gray-500'>No adverts found</p>
+				)}
+			</div>
+
+			{/* Pagination Section */}
+			<div className='flex justify-center my-10'>
+				<PaginatedComponent
+					total={totalAdverts}
+					initialPage={currentPage}
+					initialLimit={limit}
+					fetch={getAdverts}
+				/>
+			</div>
+		</div>
+	)
+}
+
 export default CampaignStats
+
+
+
+
+
