a4a42f61bbbf7b579315a6064033b57fd7e602ec
BelocatedLtd <belocatedltd@gmail.com>
2024-10-24 15:24:00 +0100

    Update AdItem.tsx

--- src/components/dashboard/AdItem.tsx
+++ src/components/dashboard/AdItem.tsx
@@ -51,12 +51,15 @@ const AdItem = ({
 }: AdItemProps) => {
   const [payBtn, setPayBtn] = useState('Pay Now')
   const [toggleTaskPerformers, settoggleTaskPerformers] = useState(false)
+  const [toggleTaskProofModal, setToggleTaskProofModal] = useState(false)
+  const [taskProof, setTaskProof] = useState(null)
+  const [openProofModal, setOpenProofModal] = useState(false)
+  const [selectedProof, setSelectedProof] = useState<string | null>(null)
+
   const dispatch = useDispatch()
   const isError = useSelector(selectIsError)
   const isSuccess = useSelector(selectIsSuccess)
   const isLoading = useSelector(selectIsLoading)
-  const [toggleTaskProofModal, setToggleTaskProofModal] = useState(false)
-  const [taskProof, setTaskProof] = useState()
 
   useEffect(() => {
     if (status === 'Pending') {
@@ -72,64 +75,54 @@ const AdItem = ({
 
   const handleToggleTaskPerformers = (e: any) => {
     e.preventDefault()
-
-    if (taskSubmitters && taskSubmitters.length === 0) {
+    if (taskSubmitters?.length === 0) {
       toast.error('No Task Submitted')
       return
     }
-
-    settoggleTaskPerformers(!toggleTaskPerformers)
+    settoggleTaskPerformers((prev) => !prev)
   }
 
-  function openPopup(e: any, tp: any) {
+  const handleTaskApproval = async (e: any, clickedTask: any) => {
     e.preventDefault()
 
-    setTaskProof(tp)
-    setToggleTaskProofModal(!toggleTaskProofModal)
+    if (clickedTask.status === 'Approved') {
+      toast.success('Task has already been approved')
+      return
+    }
+
+    if (!clickedTask?._id) {
+      toast.error('Task information missing')
+      return
+    }
+
+    dispatch(
+      handleApproveTask({
+        taskId: clickedTask._id,
+        status: 'Approved',
+        message: 'The advertiser approved this task',
+      }) as any
+    )
+
+    if (isError) {
+      toast.error('Error Approving Task')
+      return
+    }
+
+    if (isSuccess) {
+      toast.success('Task Approved')
+      callback()
+    }
   }
-const [openProofModal, setOpenProofModal] = useState(false);
-  const [selectedProof, setSelectedProof] = useState<string | null>(null);
 
   const handleProofClick = (proofUrl: string) => {
-    setSelectedProof(proofUrl);
-    setOpenProofModal(true);
-  };
+    setSelectedProof(proofUrl)
+    setOpenProofModal(true)
+  }
 
   const handleCloseProofModal = () => {
-    setOpenProofModal(false);
-    setSelectedProof(null);
-  };
-  const handleTaskApproval = async (e: any, clickedTask: any) => {
-    		e.preventDefault()
-    
-    		if (clickedTask.status === 'Approved') {
-    			toast.success('Task has already been approved')
-    			return
-    		}
-    
-    		const approveTaskData = {
-    			taskId: clickedTask?._id,
-    			status: 'Approved',
-    			message: 'The advertiser approved this task',
-    		}
-    
-    		if (!clickedTask?._id) {
-    			toast.error('Task information missing')
-    			return
-    		}
-    
-    		dispatch(handleApproveTask(approveTaskData) as any)
-    
-    		if (isError) {
-    			toast.error('Error Approving Task')
-    		}
-    
-    		if (isSuccess) {
-    			toast.success('Task Approved')
-    			callback()
-    		}
-    	}
-    
+    setOpenProofModal(false)
+    setSelectedProof(null)
+  }
 
   const getPaymentStatusBgColor = (status: any) => {
     switch (status) {
@@ -146,277 +139,94 @@ const [openProofModal, setOpenProofModal] = useState(false);
         return 'bg-gray-500'
     }
   }
-
   return (
-    <div onClick={handleToggleTaskPerformers} className='relative border cursor-pointer hovershadow flex w-full h-fit p-[1.5rem] rounded-2xl '>
+    <div
+      onClick={handleToggleTaskPerformers}
+      className='relative border cursor-pointer hover:shadow flex w-full p-[1.5rem] rounded-2xl'
+    >
       <Loader open={isLoading} />
 
-      {toggleTaskProofModal && (
-        <TaskProofModal toggleTaskProof={openPopup} task={taskProof} />
-      )}
-      {status === 'Pending' && (
-        <Image
-          src={close}
-          alt='close'
-          className=' text-tertiary w-[28px] h-[28px]'
-        />
-      )}
-
-      <div className="bg-white p-4 rounded-lg shadow-md mb-4 text-sm">
-        {/* Top Section: Icon, Date, Title */}
-        <div className="flex items-center justify-between">
-          {/* Icon and Date */}
-          <div className="flex items-center gap-2">
-            <Image
-              src={icons?.find((icon) => icon.platform === item.platform)?.icon}
-              alt={item.platform}
-              className="w-6 h-6"
-            />
-            <div>
-              <span className="text-gray-500">{formatDate(date)}</span>
-              <h3 className="text-base font-medium text-black">{title}</h3>
-            </div>
-          </div>
-        </div>
-
-        {/* Pricing, Completed Tasks, and Submitted Tasks */}
-        <div className="mt-2">
-          <p className="text-gray-700 text-xs">
-            <span className="font-bold">Pricing:</span> ₦{adperPostAmt} per advert
-          </p>
-          <p className="text-gray-700 text-xs">
-            <span className="font-bold">Approved Tasks:</span> {completedTasksCount}
-          </p>
-          <p className="text-gray-700 text-xs">
-            <span className="font-bold">Submitted Tasks:</span> {taskSubmitters?.length}
-          </p>
-        </div>
-
-        {/* Link and Ad Unit */}
-        <div className="mt-2">
-          <p>
-            <span className="font-medium">Link:</span>{' '}
-            <a
-              href={url}
-              className="text-blue-500 underline"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {url.slice(0, 20)}...
-            </a>
-          </p>
-          <p className="text-gray-700 text-xs">
-            <span className="font-bold">Ad Unit:</span> {roi}
-          </p>
-        </div>
-
-        {/* Location Details */}
-        <div className="mt-2 grid grid-cols-2 gap-4 text-xs">
-          <div>
-            <span className="font-bold">Gender:</span> {item?.gender}
-          </div>
-          <div>
-            <span className="font-bold">LGA:</span> {item?.lga}
-          </div>
-          <div>
-            <span className="font-bold">State:</span> {item?.state}
-          </div>
-        </div>
-
-        {/* Amount Paid and Status */}
-        <div className="mt-4 flex justify-between items-center">
-          <div className="text-lg font-bold text-xs">
-            <span>Amount Paid:</span> ₦{adBudget}
-          </div>
-          <div>
-            <span
-              className={`px-2 py-1 border border-green-500 rounded text-black ${getPaymentStatusBgColor(status)}`}
-            >
-              {status}
-            </span>
-          </div>
-        </div>
-
-        {/* View and Monitor Buttons */}
-        <div className="mt-4 flex justify-end gap-2">
-          <button className="px-3 py-1 text-white bg-blue-500 rounded hover:bg-blue-600">
-            View & Monitor Results
-          </button>
-        </div>
-
-      </div>
-      <div onClick={() => settoggleTaskPerformers(!toggleTaskPerformers)} className="border rounded-xl p-6 hover:shadow-lg">
-      {/* Task Performers Modal */}
-      <Modal
-        open={toggleTaskPerformers}
-        onClose={() => settoggleTaskPerformers(false)}
-        aria-labelledby="task-performers-modal"
-      >
-        <div className="fixed inset-0 bg-white z-50 p-8 overflow-y-auto">
-          <button
-            onClick={() => settoggleTaskPerformers(false)}
-            className="absolute top-4 right-4 p-2 bg-gray-100 rounded-full hover:bg-gray-200"
-          >
-            ✕
-          </button>
-
-          {taskSubmitters?.map((tp: any) => (
-            <div key={tp._id} className="border-b py-6">
-              <h3 className="font-bold">Allocation Result</h3>
-              <div className="flex justify-between items-center my-4">
-                <div>
-                  <p className="text-sm">@{tp?.taskPerformerId?.username}</p>
-                  <p className="text-sm">{tp?.taskPerformerId?.fullname}</p>
-                  <p className="text-xs text-gray-500">{formatDate(tp.createdAt)}</p>
-                </div>
-
-                <button
-                  onClick={(e) => handleTaskApproval(e, tp)}
-                  className={`px-4 py-2 rounded-md text-white ${
-                    tp.status === 'Approved' ? 'bg-green-500' : 'bg-yellow-500 hover:bg-yellow-600'
-                  }`}
-                >
-                  {tp.status === 'Approved' ? 'Approved' : 'Approve'}
-                </button>
-              </div>
-
-              <div className="flex justify-between items-center text-sm">
-                <div>
-                  <label>Social Media:</label>{' '}
-                  <a
-                    href={tp.socialPageLink}
-                    className="text-blue-500 hover:underline"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {tp.socialPageLink.slice(0, 13)}...
-                  </a>
-                </div>
-
-                <div>
-                  <label>Status:</label> {tp.status}
-                </div>
-              </div>
-
-              <div className="mt-2">
-                <label>Proof:</label>{' '}
-                {tp.proofOfWorkMediaURL?.[0]?.secure_url ? (
-                  <span
-                    onClick={() => handleProofClick(tp.proofOfWorkMediaURL[0].secure_url)}
-                    className="text-blue-500 hover:text-red-500 cursor-pointer"
-                  >
-                    View Proof
-                  </span>
-                ) : (
-                  'N/A'
-                )}
-              </div>
-            </div>
-          ))}
-        </div>
-      </Modal>
-
-      {/* Proof Modal */}
-      <Modal open={openProofModal} onClose={handleCloseProofModal} aria-labelledby="proof-modal">
-        <div className="flex items-center justify-center min-h-screen">
-          <div className="bg-white p-6 rounded-lg shadow-md">
+      <Modal open={openProofModal} onClose={handleCloseProofModal}>
+        <div className='flex items-center justify-center min-h-screen'>
+          <div className='bg-white p-6 rounded-lg shadow-md'>
             {selectedProof ? (
-              <Image src={selectedProof} alt="Proof" width={600} height={600} className="rounded-md" />
+              <Image
+                src={selectedProof}
+                alt='Proof'
+                width={600}
+                height={600}
+                className='rounded-md'
+              />
             ) : (
               'No proof available.'
             )}
             <button
               onClick={handleCloseProofModal}
-              className="mt-4 bg-gray-500 text-white px-4 py-2 rounded"
+              className='mt-4 bg-gray-500 text-white px-4 py-2 rounded'
             >
               Close
             </button>
           </div>
         </div>
       </Modal>
-    </div>
 
-
-
-      {/* <Modal
-          open={toggleTaskPerformers}
-          onClose={() => {
-            settoggleTaskPerformers(false)
-          }}
-          aria-labelledby='modal-modal-title'
-          aria-describedby='modal-modal-description'>
-          <div className='flex flex-col justify-center items-center h-full '>
-            <div className='relative  w-[85%] md:w-[600px] h-1/2 overflow-y-auto md:py-[2rem] rounded-2xl p-4 bg-primary'>
-              {taskSubmitters?.map((tp: any) => (
-                <div className='w-full border-gray-200 py-[1rem]' key={tp._id}>
-                  <div className='task performer details mb-[1rem] flex justify-between w-full gap-1'>
-                    <div>
-                      <small className='text-gray-400 font-semibold'>
-                        Perfomer Username: @{tp?.taskPerformerId?.username}
-                      </small>
-                      <h3 className='font-bold text-gray-600'>
-                        Perfomer Fullname: {tp?.taskPerformerId?.fullname}
-                      </h3>
-                      <span className='text-gray-400 font-semibold text-[9px]'>
-                        {formatDate(tp?.createdAt)}
-                      </span>
-                    </div>
-
-                    <div className='flex items-center gap-2'>
-                      <button
-                        onClick={(e) => handleTaskApproval(e, tp)}
-                        className={`
-                                ${tp?.status === 'Approved'
-                            ? 'bg-green-600'
-                            : 'bg-yellow-600'
-                          } 
-                                flex items-center gap-2 rounded-2xl px-3 py-2 text-primary hover:bg-orange-400`}>
-                        {tp?.status === 'Approved' ? 'Approved' : 'Approve'}
-                        <span>{isLoading && <LoaderIcon />}</span>
-                      </button>
-                    </div>
-                  </div>
-
-                  <div className='flex flex-col gap-3 md:items-center justify-between mb-[1rem] md:flex-row'>
-                    <div className='first columns flex flex-col'>
-                      <label className='font-bold'>Social Media</label>
-                      <a
-                        href={tp?.socialPageLink}
-                        className='text-blue-600 hover:text-red-600 cursor-pointer'>
-                        {tp.socialPageLink.slice(0, 20)}...
-                      </a>
-                    </div>
-
-                    <div className='third columns'>
-                      <label className='font-bold'>Status</label>
-                      <p className='flex items-center gap-1'>
-                        <span>
-                          <CheckmarkIcon />
-                        </span>
-                        {tp?.status}
-                      </p>
-                    </div>
-                  </div>
-
-                  <div className='second columns flex mt-4 flex-col'>
-                    <label className='font-bold'>Proof</label>
-                    {tp?.proofOfWorkMediaURL?.[0]?.secure_url ? (
-                      <Image
-                        src={tp?.proofOfWorkMediaURL?.[0]?.secure_url}
-                        alt='proof'
-                        width={300}
-                        height={300}
-                      />
-                    ) : (
-                      'N/A'
-                    )}
-                  </div>
-                </div>
-              ))}
+      <div className='bg-white p-4 rounded-lg shadow-md'>
+        <div className='flex items-center justify-between'>
+          <div className='flex items-center gap-2'>
+            <Image
+              src={icons.find((icon) => icon.platform === item.platform)?.icon}
+              alt={item.platform}
+              className='w-6 h-6'
+            />
+            <div>
+              <span className='text-gray-500'>{formatDate(date)}</span>
+              <h3 className='text-base font-medium'>{title}</h3>
             </div>
           </div>
-        </Modal> */}
+        </div>
 
+        <div className='mt-2'>
+          <p className='text-gray-700'>
+            <span className='font-medium'>Pricing:</span> ₦{adperPostAmt} per advert
+          </p>
+          <p className='text-gray-700'>
+            <span className='font-semi-bold'>Approved Tasks:</span> {completedTasksCount}
+          </p>
+          <p className='text-gray-700'>
+            <span className='font-medium'>Submitted Tasks:</span> {taskSubmitters?.length}
+          </p>
+        </div>
+
+        <div className='mt-2'>
+          <p>
+            <span className='font-medium'>Link:</span>{' '}
+            <a
+              href={url}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-blue-500 underline'
+            >
+              {url.slice(0, 20)}...
+            </a>
+          </p>
+          <p className='text-gray-700'>
+            <span className='font-medium'>Ad Unit:</span> {roi}
+          </p>
+        </div>
+
+        <div className='mt-4 flex justify-between'>
+          <div className='text-lg font-medium'>Amount Paid: ₦{adBudget}</div>
+          <span className={`px-2 py-1 rounded ${getPaymentStatusBgColor(status)}`}>
+            {status}
+          </span>
+        </div>
+
+        <div className='mt-4 flex justify-end'>
+          <button className='px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600'>
+            {payBtn}
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
