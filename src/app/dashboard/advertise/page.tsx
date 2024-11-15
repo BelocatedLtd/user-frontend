988fc867ebaffd2397d29bc3d593e6425c7b74c1
BelocatedLtd <belocatedltd@gmail.com>
2024-11-15 10:59:05 +0100

    Update page.tsx spotify

--- src/app/dashboard/advertise/page.tsx
+++ src/app/dashboard/advertise/page.tsx
@@ -134,7 +134,7 @@ const Advertise = () => {
 								<div className='absolute bg-white space-y-4 border   max-h-72  overflow-scroll'>
 									{selectedPlatformObject?.assetplatform === menu.value &&
 									toggleServices ? (
-										<ul className=' mt-2 overflow-scroll rounded-sm absolute z-100 pb-2'>
+										<ul className=' mt-2 overflow-scroll rounded-sm relative z-100 pb-2'>
 											{selectedPlatformObject?.assets?.map(
 												(service: Asset, index: number) => (
 													<li
