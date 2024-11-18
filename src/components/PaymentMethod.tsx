dedffd7f489618a7ad5228655225cea80af1b094
BelocatedLtd <belocatedltd@gmail.com>
2024-11-18 21:47:12 +0100

    Update PaymentMethod.tsx

--- src/components/PaymentMethod.tsx
+++ src/components/PaymentMethod.tsx
@@ -80,7 +80,6 @@ const PaymentMethod = ({
 	paymentFormData.append('platform', platform)
 	paymentFormData.append('service', service)
 	paymentFormData.append('adTitle', adTitle)
-	paymentFormData.append('TD', TD)
 	paymentFormData.append('desiredROI', desiredROI)
 	paymentFormData.append('gender', gender)
 	paymentFormData.append('state', state)
