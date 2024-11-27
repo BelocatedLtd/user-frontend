29f8129f38a7ba626b3e54ceda1dc95a5968b3db
BelocatedLtd <belocatedltd@gmail.com>
2024-11-27 13:47:43 +0100

    Update submitTask.tsx emit message

--- src/components/dashboard/submitTask.tsx
+++ src/components/dashboard/submitTask.tsx
@@ -148,7 +148,7 @@ const TaskSubmit = ({
 			// Notify via WebSocket
 			socket.emit('sendActivity', {
 				userId: user?.id,
-				action: `@${user?.username} just performed a task on ${task?.platform}`,
+				action: `@${user?.username} just performed a task on ${task?.platform} to earn ${task.toEarn}`,
 			});
 			onClose();
 				router.push(`/dashboard/earn/success?platform=${task.platform}`);
