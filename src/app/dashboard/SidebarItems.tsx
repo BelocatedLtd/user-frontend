ab34773c5fbac0b22417d93c8e8a51b4c3de21f5
BelocatedLtd <belocatedltd@gmail.com>
2024-11-30 22:21:22 +0100

    Update SidebarItems.tsx

--- src/app/dashboard/SidebarItems.tsx
+++ src/app/dashboard/SidebarItems.tsx
@@ -21,11 +21,13 @@ const SidebarItems = ({ item, isOpen, isMobile }: TSidebarItems) => {
 	return (
 		<Link href={item.path}>
 			<div className={cn('sidebar-item flex flex-col items-center', { active: isActive })}>
-				{/* Icon */}
-				<div className="icon text-2xl text-gray-700 text-center items-center">{item.icon}</div>
+				{/* Centered Icon */}
+				<div className="flex justify-center items-center text-2xl text-gray-700">
+					{item.icon}
+				</div>
 				{/* Title */}
 				<div
-					className={`title text-xs text-gray-500 mt-1 text-center ${
+					className={`text-xs text-gray-500 mt-1 text-center ${
 						!isMobile && !isOpen ? 'hidden' : ''
 					}`}
 				>
