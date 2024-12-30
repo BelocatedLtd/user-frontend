019b25dcf5e5da880dd6bef651ac626c1e853f1b
BelocatedLtd <belocatedltd@gmail.com>
2024-12-30 21:38:15 +0100

    Update page.tsx design

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -247,30 +247,30 @@ const Dashboard = () => {
 		{/* Task Section */}
 		<div style={{borderTopLeftRadius:"50px", boxShadow:"5px 5px 5px rgb(153, 153, 153) inset", padding: '0 10px', marginLeft:'-10px' }} className="text-center pb-4">
 			{/* Task Count */}
-			<div className="text-[16px] sm:text-[12px] font-semibold text-gray-700 pb-[30px] ">
+			<div className="text-[16px] sm:text-[12px] font-semibold text-gray-700 p-[30px] ">
 				You have {totalTasks} available tasks to complete.
 			</div>
 	
 			{/* Task Summary Box */}
 			<div className="bg-white rounded-md p-6 mt-4 font-bold" style={{backgroundColor:"rgb(212, 231, 248)", margin: '0 30px 10px 30px', textAlign:"left"}}>
 				{/* Approved and Completed Tasks */}
-				<div className="text-[20px] sm:text-[15px] font-normal text-gray-700">
+				<div className="text-[15px] sm:text-[15px] font-normal text-gray-700">
 					<strong> {approvedTasks} </strong> Approved of <strong> {completedTasks + approvedTasks} </strong> Completed Tasks
 				</div>
 				{/* Remaining Tasks */}
-				<div className="text-[18px] sm:text-[15px] font-normal text-gray-600 mt-2">
+				<div className="text-[15px] sm:text-[15px] font-normal text-gray-600 mt-2">
 					Remaining Task to Perform:  <strong> {remainingTasksToComplete} </strong>
 				</div>
 				{/* Tasks Waiting Approval */}
-				<div className="text-[18px] sm:text-[15px] font-normal text-gray-600 mt-2">
+				<div className="text-[15px] sm:text-[15px] font-normal text-gray-600 mt-2">
 					<strong> {remainingTasksToApprove} </strong> Tasks waiting Approval
 				</div>
 			</div>
 		</div>
 	
 		{/* Click Here Button (Full Width) */}
-		<div className="w-full pt-5">
-			<button style={{boxShadow:"0px 8px 8px 0px rgba(113, 199, 239, 0.8)", borderRadius:"100px"}} onClick={() => router.push('/dashboard/earn')} className="w-full bg-blue-500 text-white py-3 font-semibold text-lg rounded-md hover:bg-blue-600 transition duration-300">
+		<div className="w-[95%] pt-5 ml-0" style={{ marginLeft: "2%" }}>
+			<button onClick={() => router.push('/dashboard/earn')} className="w-[95%] bg-blue-500 text-white py-3 font-semibold text-lg rounded-md hover:bg-blue-600 transition duration-300" style={{boxShadow:"0px 8px 8px 0px rgba(113, 199, 239, 0.8)"}}>
 				Click Here To
 				Perform Task and Earn
 			</button>
@@ -278,47 +278,47 @@ const Dashboard = () => {
 	
 		{/* Three Buttons Side by Side */}
 		<div className="flex space-x-4 mt-6">
-			<button style={{boxShadow:"0px 8px 8px 0px rgba(113, 199, 239, 0.8)"}}className="text-black px-6 py-2 font-normal text-lg rounded-full transition duration-300 shadow-md" onClick={handleAdvertise}>
+			<button style={{boxShadow:"0px 8px 8px 0px rgba(113, 199, 239, 0.8)", background:'linear-gradient(to bottom, #528EF3 0%, #FFFFFF 100%)', width:'40%'}} className="text-black px-6 py-2 font-normal text-[14px] rounded-full transition duration-300 shadow-md" onClick={handleAdvertise}>
 				Advertise
 			</button>
-			<button style={{boxShadow:"0px 8px 8px 0px rgba(113, 199, 239, 0.8)"}} className="text-black px-6 py-2 font-normal text-lg rounded-full transition duration-300 shadow-md" onClick={() => router.push('/dashboard/wallet')}>
+			<button style={{boxShadow:"0px 8px 8px 0px rgba(113, 199, 239, 0.8)", background:'linear-gradient(to bottom, #528EF3 0%, #FFFFFF 100%)',width:'40%'}} className="text-black px-6 py-2 font-normal text-[14px] rounded-full transition duration-300 shadow-md" onClick={() => router.push('/dashboard/wallet')}>
 				My Wallet
 			</button>
-			<button style={{boxShadow:"0px 8px 8px 0px rgba(113, 199, 239, 0.8)"}} className="text-black px-6 py-2 font-normal text-lg rounded-full transition duration-300 shadow-md" onClick={() => router.push('/dashboard/referral')}>
+			<button style={{boxShadow:"0px 8px 8px 0px rgba(113, 199, 239, 0.8)", background:'linear-gradient(to bottom, #528EF3 0%, #FFFFFF 100%)',width:'40%'}} className="text-black px-6 py-2 font-normal text-[14px] rounded-full transition duration-300 shadow-md" onClick={() => router.push('/dashboard/referral')}>
 				Referral Program
 			</button>
 		</div>
 	
 		{/* Four Small Cards (2 rows of 2) */}
-		<div className="grid grid-cols-2 gap-4 mt-6">
+		<div className="grid grid-cols-2 gap-4 mt-6 p-7">
 			{/* Card 1 */}
-			<div className="border border-[rgb(71,71,209)] bg-white rounded-md p-6 text-center">
+			<div className="border border-[rgb(71,71,209)] bg-white  p-6 text-center" style={{borderRadius:'30px'}}>
 				<h3 className="font-normal text-sm text-black-700">Total Earnings</h3>
-				<p className="text-gray-500 mt-2"><strong className='text-xl md:text-3xl'>
+				<p className="text-black-500 mt-2"><strong className='text-lg md:text-2xl'>
 					{toNaira(dashboardData?.totalEarnings.value ?? 0)}
 				</strong></p>
 			</div>
 			{/* Card 2 */}
-			<div className="border border-[rgb(71,71,209)] bg-white rounded-md p-6 text-center">
+			<div className="border border-[rgb(71,71,209)] bg-white p-6 text-center" style={{borderRadius:'30px'}}>
 				<h3 className="font-normal text-sm text-black-700">My Balance</h3>
-				<p className="text-black-500 mt-2">{toNaira(dashboardData?.myBalance.value ?? 0)}</p>
+				<p className="text-black-500 mt-2"><strong className='text-lg md:text-2xl'>{toNaira(dashboardData?.myBalance.value ?? 0)}</strong></p>
 			</div>
 			{/* Card 3 */}
-			<div className="border border-[rgb(71,71,209)] bg-white rounded-md p-6 text-center">
+			<div className="border border-[rgb(71,71,209)] bg-white p-6 text-center" style={{borderRadius:'30px'}}>
 				<h3 className="font-normal text-sm text-black-700">Adverts Created</h3>
-				<p className="text-black-500 mt-2"><strong className='text-xl md:text-3xl'>
+				<p className="text-black-500 mt-2"><strong className='text-lg md:text-2xl'>
 					{dashboardData?.advertsCreated.value ?? 0}
 				</strong></p>
 			</div>
 			{/* Card 4 */}
-			<div className="border border-[rgb(71,71,209)] bg-white rounded-md p-6 text-center">
-				<h3 className="font-normal text-xl text-gray-700">Tasks Completed</h3>
-				<p className="text-gray-500 mt-2">{completedTasks + approvedTasks}</p>
+			<div className="border border-[rgb(71,71,209)] bg-white p-6 text-center" style={{borderRadius:'30px'}}>
+				<h3 className="font-normal text-sm text-gray-700">Tasks Completed</h3>
+				<p className="text-black-500 mt-2"><strong className='text-lg md:text-2xl'>{completedTasks + approvedTasks}</strong></p>
 			</div>
 		</div>
 	
 		<div className='border p-6 col-span-2 border-gray-200 rounded-lg '>
-		<div style={{backgroundColor:"rgb(71, 71, 209)" }} className="text-white font-bold py-2 px-4 pt-4 rounded text-[18px] sm:text-[12px] text-[13px]">Referral</div>
+		<div className="text-white font-bold py-2 px-4 pt-4 rounded text-[18px] sm:text-[12px] text-[13px]">Referral</div>
 			<ReferralsTable />
 		</div>
 	
