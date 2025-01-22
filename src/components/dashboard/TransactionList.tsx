cad1d03ec3ff43a08f45dbcd55c14c757e343e52
BelocatedLtd <belocatedltd@gmail.com>
2025-01-22 02:39:02 +0100

    Update TransactionList.tsx added proof

--- src/components/dashboard/TransactionList.tsx
+++ src/components/dashboard/TransactionList.tsx
@@ -4,7 +4,6 @@ import DataTable from 'react-data-table-component'
 import { toast } from 'react-hot-toast'
 import { MdArrowDownward } from 'react-icons/md'
 import { useDispatch, useSelector } from 'react-redux'
-//import { format } from 'date-fns';
 import {
 	handleGetUserTransactions,
 	selectIsError,
@@ -23,31 +22,67 @@ const TransactionList = () => {
 	const [currentPage, setCurrentPage] = useState(1)
 	const [totalRows, setTotalRows] = useState(0)
 	const [rowsPerPage, setRowsPerPage] = useState(10)
-
 	const [transactions, setTransactions] = useState([])
+	const [isModalOpen, setIsModalOpen] = useState(false)
+	const [modalContent, setModalContent] = useState('')
+  
+  const handleProofClick = (url) => {
+		setModalContent(url);
+		setIsModalOpen(true);
+	  };
+  
+  const closeModal = () => {
+  setIsModalOpen(false);
+  setModalContent('');
+};
+
 
 	const columns = [
 		{
 			name: 'S/N',
-			selector: (row: { trxId: any }) => row.trxId,
+			selector: (row) => row.trxId,
 		},
 		{
 			name: 'Transaction Type',
-			selector: (row: { trxType: any }) => row.trxType,
+			selector: (row) => row.trxType,
 		},
 		{
 			name: 'Amount',
-			selector: (row: { chargedAmount: any }) => row.chargedAmount,
+			selector: (row) => row.chargedAmount,
 			sortable: true,
 		},
 		{
 			name: 'Date',
-			selector: (row: { date: any }) => row.date ? new Date(parseInt(row.date)).toLocaleString() : 'N/A',
+			selector: (row) =>
+				row.date ? new Date(parseInt(row.date)).toLocaleString() : 'N/A',
 		},
 		{
 			name: 'Status',
-			selector: (row: { status: any }) => row.status,
+			selector: (row) => row.status,
 			sortable: true,
+		},
+		{
+			name: 'View Proof',
+			cell: (row) =>
+				row.trxType === 'bank transfer' ? (
+					<div className="mt-2">
+						<label>Proof:</label>{' '}
+						{row.proofOfWorkMediaURL?.[0]?.secure_url ? (
+							<span
+								onClick={() =>
+									handleProofClick(row.proofOfWorkMediaURL[0].secure_url)
+								}
+								className="text-blue-500 hover:text-red-500 cursor-pointer"
+							>
+								View Proof
+							</span>
+						) : (
+							'N/A'
+						)}
+					</div>
+				) : (
+					'N/A'
+				),
 		},
 	]
 
@@ -61,14 +96,9 @@ const TransactionList = () => {
 		},
 	}
 
-	//   const handleButtonClick = (e, artisanId) => {
-	//     e.preventDefault();
-	//     router.push(`/dashboard/artisan/${artisanId}`)
-	//   }
-
-	const fetchTransactions = async (page: number, rows: number) => {
+	const fetchTransactions = async (page, rows) => {
 		const response = await dispatch(
-			handleGetUserTransactions({ page, limit: rows }) as any,
+			handleGetUserTransactions({ page, limit: rows })
 		)
 		if (response.payload) {
 			setTotalRows(response.payload.totalTransactions)
@@ -84,41 +114,69 @@ const TransactionList = () => {
 		}
 	}, [isError, dispatch])
 
-	const handlePageChange = (page: number) => {
+	const handlePageChange = (page) => {
 		setCurrentPage(page)
 		fetchTransactions(page, rowsPerPage)
 	}
 
-	const handleChangeRowsPerPage = (rowsPerPage: number) => {
+	const handleChangeRowsPerPage = (rowsPerPage) => {
 		setRowsPerPage(rowsPerPage)
 		fetchTransactions(currentPage, rowsPerPage)
 	}
 
+	const handleProofClick = (url) => {
+		setModalContent(url)
+		setIsModalOpen(true)
+	}
+
+	const closeModal = () => {
+		setIsModalOpen(false)
+		setModalContent('')
+	}
+
 	return (
-		// <DataTable
-		// columns={columns}
-		// data={transactions}
-		// progressPending={isLoading}
-		// pagination
-		// selectableRows
-		// fixedHeader
-		// customStyles={customStyles}
-		// //handleButtonClick={handleButtonClick}
-		// />
-		<DataTable
-			columns={columns as any}
-			data={transactions}
-			progressPending={isLoading}
-			pagination
-			paginationServer
-			paginationTotalRows={totalRows}
-			onChangePage={handlePageChange}
-			onChangeRowsPerPage={handleChangeRowsPerPage}
-			selectableRows
-			fixedHeader
-			customStyles={customStyles}
-			sortIcon={sortIcon}
-		/>
+		<>
+			<DataTable
+				columns={columns}
+				data={transactions}
+				progressPending={isLoading}
+				pagination
+				paginationServer
+				paginationTotalRows={totalRows}
+				onChangePage={handlePageChange}
+				onChangeRowsPerPage={handleChangeRowsPerPage}
+				selectableRows
+				fixedHeader
+				customStyles={customStyles}
+				sortIcon={sortIcon}
+			/>
+			{isModalOpen && (
+				<div
+					className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
+					onClick={closeModal}
+				>
+					<div
+						className="bg-white p-5 rounded-md shadow-lg relative"
+						style={{ width: '80%', height: '80%', maxWidth: '800px' }}
+						onClick={(e) => e.stopPropagation()}
+					>
+						<button
+							className="absolute top-2 right-2 text-gray-500 hover:text-red-500 p-3"
+							onClick={closeModal}
+							style={{ backgroundColor: 'red', color: 'white' }}
+						>
+							Close
+						</button>
+						<img
+							src={modalContent}
+							alt="Proof of Work"
+							className="w-full h-full object-contain rounded-md"
+							style={{ width: '100%', height: '100%' }}
+						/>
+					</div>
+				</div>
+			)}
+		</>
 	)
 }
 
